15ee24402f4de294d881a9330f709c623b86fbe8
Sarthak Arsul <83406721+sarthakarsul18@users.noreply.github.com>
2026-07-01 13:26:55 +0530

    Add solution - CodeHub

---
+++ 8-kyu/i-love-you-a-little-a-lot-passionately-dot-dot-dot-not-at-all/i-love-you-a-little-a-lot-passionately-dot-dot-dot-not-at-all.py
@@ -0,0 +1,11 @@
+def how_much_i_love_you(nb_petals):
+    phrase = [
+        "I love you",
+        "a little",
+        "a lot",
+        "passionately",
+        "madly",
+        "not at all"
+    ]
+​
+    return phrase[(nb_petals - 1) % 6]

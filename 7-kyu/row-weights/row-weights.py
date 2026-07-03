60ba6d889f7ec2a7753b052baa2d927f4e44f49c
Sarthak Arsul <83406721+sarthakarsul18@users.noreply.github.com>
2026-07-03 13:39:09 +0530

    Add solution - CodeHub

---
+++ 7-kyu/row-weights/row-weights.py
@@ -0,0 +1,11 @@
+def row_weights(array):
+    team1 = 0
+    team2 = 0
+​
+    for i in range(len(array)):
+        if i % 2 == 0:
+            team1 += array[i]
+        else:
+            team2 += array[i]
+​
+    return (team1, team2)

c0fb81837d6ad5537de247fb82cbd68e81186e44
Sarthak Arsul <83406721+sarthakarsul18@users.noreply.github.com>
2026-07-03 12:18:10 +0530

    Add solution - CodeHub

---
+++ 6-kyu/tortoise-racing/tortoise-racing.py
@@ -0,0 +1,11 @@
+def race(v1, v2, g):
+    if v1 >= v2:
+        return None
+​
+    total_seconds = (g * 3600) // (v2 - v1)
+​
+    hours = total_seconds // 3600
+    minutes = (total_seconds % 3600) // 60
+    seconds = total_seconds % 60
+​
+    return [hours, minutes, seconds]

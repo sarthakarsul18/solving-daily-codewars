4f8139b1adc88a08582f694d53d4ccb0aa9fe929
Sarthak Arsul <83406721+sarthakarsul18@users.noreply.github.com>
2026-07-05 12:42:04 +0530

    Add solution - CodeHub

---
+++ 8-kyu/simple-calculator/simple-calculator.py
@@ -0,0 +1,16 @@
+def calculator(x, y, op):
+    if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
+        return "unknown value"
+​
+    if op == "+":
+        return x + y
+    elif op == "-":
+        return x - y
+    elif op == "*":
+        return x * y
+    elif op == "/":
+        if y == 0:
+            return "unknown value"
+        return x / y
+    else:
+        return "unknown value"
